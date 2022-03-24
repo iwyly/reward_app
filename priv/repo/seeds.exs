235da0c79bf25baa79f4f29e29d33683454500eb
task01 <87199698+iwyly@users.noreply.github.com>
2022-03-24 19:16:12 +0100

    added seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,17 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     RewardApp.Repo.insert!(%RewardApp.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+RealEstate.Accounts.register_admin(%{
+  email: "admin@company.com",
+  password: "123456789abc",
+  password_confirmation: "123456789abc"
+})
+
+RealEstate.Accounts.register_user(%{
+  email: "bob@company.com",
+  password: "123456789abc",
+  password_confirmation: "123456789abc"
+})
+
+RealEstate.Accounts.register_user(%{
+  email: "john@company.com",
+  password: "123456789abc",
+  password_confirmation: "123456789abc"
+})
