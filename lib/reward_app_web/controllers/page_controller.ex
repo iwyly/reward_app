6734957795cc3998aa5e37aa359c9f8d053df063
task01 <87199698+iwyly@users.noreply.github.com>
2022-03-25 22:26:52 +0100

    small changes to pagecontroller

--- lib/reward_app_web/controllers/page_controller.ex
+++ lib/reward_app_web/controllers/page_controller.ex
@@ -1,14 +1,14 @@
 defmodule RewardAppWeb.PageController do
   use RewardAppWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "index.html")
+  def member(conn, _params) do
+    render(conn, "member.html")
   end
   def admin(conn, _params) do
     render(conn, "admin.html")
   end
 
   def welcome(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "welcome.html")
   end
 end
