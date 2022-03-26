d46cd2202ed2f9403f85c1c6b90cfcf624de9702
task01 <87199698+iwyly@users.noreply.github.com>
2022-03-26 17:22:58 +0100

    seeds fix

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,16 +1,16 @@
-RealEstate.Accounts.register_admin(%{
+RewardApp.Accounts.register_admin(%{
   email: "admin@company.com",
   password: "123456789abc",
   password_confirmation: "123456789abc"
 })
 
-RealEstate.Accounts.register_user(%{
+RewardApp.Accounts.register_user(%{
   email: "bob@company.com",
   password: "123456789abc",
   password_confirmation: "123456789abc"
 })
 
-RealEstate.Accounts.register_user(%{
+RewardApp.Accounts.register_user(%{
   email: "john@company.com",
   password: "123456789abc",
   password_confirmation: "123456789abc"
